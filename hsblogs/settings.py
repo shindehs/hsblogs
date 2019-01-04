5539557c16dbe2385e24779f7856fecb96b110b1
Hanmant Shinde <hanmant.shinde@harbingergroup.com>
2019-01-04 15:41:40 +0530

    My Django First App for blogs

--- hsblogs/settings.py
+++ hsblogs/settings.py
@@ -75,18 +75,25 @@ WSGI_APPLICATION = 'hsblogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default':{
+#         # 'ENGINE':'django.db.backends.sqlite3',
+#         # 'NAME':db_path,
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'hsblogs',
+#         'USER': 'postgres',
+#         'PASSWORD':'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+#  }
+
 DATABASES = {
-    'default':{
-        # 'ENGINE':'django.db.backends.sqlite3',
-        # 'NAME':db_path,
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hsblogs',
-        'USER': 'postgres',
-        'PASSWORD':'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
- }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
